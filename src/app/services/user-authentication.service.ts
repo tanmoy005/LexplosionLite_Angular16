2bc9027970bd95e9b51d01d69277ceb5c65e9528
mrinalElogix72 <mrinal@elogixmail.com>
2024-05-08 16:55:14 +0530

    8th_May_Afternoon_Commit_Mrinal

--- src/app/services/user-authentication.service.ts
+++ src/app/services/user-authentication.service.ts
@@ -7,10 +7,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UserAuthenticationService {
   url =' https://enginebackend.komrisk.com:3000/v1/company'
+  loginURL='https://enginebackend.komrisk.com:3000/v1/login'
+
   constructor(private http: HttpClient ) { }
 
   userRegistration(data: any){
     console.log('user registration data',data)
     return this.http.post(this.url,data)
   }
+
+  userLogin(data:any){
+    var message;
+    this.http.post(this.loginURL,data).subscribe((data) =>{
+      message = data;
+    })
+    return message;
+    // return this.http.post(this.loginURL,data)
+  }
+
 }
