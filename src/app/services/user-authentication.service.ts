95e9cc0e52a430e30ff3d6478e7bbca1ff50cafe
elogixDibyo <dibyojit@elogixmail.com>
2024-05-13 19:06:28 +0530

    apiservices incomplete

--- src/app/services/user-authentication.service.ts
+++ src/app/services/user-authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
 
 
 @Injectable({
@@ -10,7 +11,7 @@ export class UserAuthenticationService {
   CreateBusinessURL = 'https://enginebackendqa.komrisk.com/v1/login'
   loginURL = 'https://enginebackendqa.komrisk.com/v1/login'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private apiService:ApiService) { }
 
   userRegistration(data: any) : Observable<any>{
     console.log('user registration data', data)
@@ -19,7 +20,8 @@ export class UserAuthenticationService {
 
   userLogin(data: any): Observable<any> {
     // Make the HTTP POST request and return the observable
-    return this.http.post(this.loginURL, data);
+    // return this.http.post(this.loginURL, data);
+    return this.apiService.postLoginData(data);
   }
   //   this.http.post(this.loginURL, data).subscribe((response) => {
   //     message = response;
