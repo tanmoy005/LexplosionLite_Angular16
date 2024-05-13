0869b5e95c6c53955535b904cd47d30a1be49df6
mrinalElogix72 <mrinal@elogixmail.com>
2024-05-13 13:12:37 +0530

    Mrinal_13th_May_Commit_1.12PM

--- src/app/user-onboard/op-unit-details-page/op-unit-details-page.component.ts
+++ src/app/user-onboard/op-unit-details-page/op-unit-details-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { treeDataitem } from 'src/app/shared/menu-items/tree-items';
+import { TreeStructureComponent } from '../common-components/tree-structure/tree-structure.component';
 @Component({
   selector: 'app-op-unit-details-page',
   templateUrl: './op-unit-details-page.component.html',
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 export class OpUnitDetailsPageComponent {
   receivedData: any;
   entityDetails: any;
-
+  
   constructor(private router: Router) {
     // Retrieve data from navigation state
     const navigation = this.router.getCurrentNavigation();
@@ -20,6 +21,7 @@ export class OpUnitDetailsPageComponent {
       //console.log(this.entityDetails)
     }
   }
+  treeDataItem = treeDataitem;
 }
 
 
