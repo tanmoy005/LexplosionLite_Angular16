9ce19876f7666cb267d1069eb65dc702404e9af1
elogixDibyo <dibyojit@elogixmail.com>
2024-05-13 18:18:50 +0530

    removed login route from authnetication routing

--- src/app/authentication/authentication.routing.ts
+++ src/app/authentication/authentication.routing.ts
@@ -1,22 +1,17 @@
 import { Routes } from '@angular/router';
 
 import { AppSideRegisterComponent } from './register/register.component';
-import { AppSideLoginComponent } from '../guest-user/login/login.component';
-
 import { VerifyEmailPageComponent } from './register/verify-email-page/verify-email-page.component';
 
 export const AuthenticationRoutes: Routes = [
- 
-  }, {
-    path: 'login',
-    component: AppSideLoginComponent,
+
   {
     path: 'register',
     component: AppSideRegisterComponent,
   },
   {
     path: 'verify-email',
-    component: VerifyEmailPageComponent,
+    component: VerifyEmailPageComponent
   },
 
 ];
