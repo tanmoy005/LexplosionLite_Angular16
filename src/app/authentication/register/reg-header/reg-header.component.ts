17d6ff8472e1264ccf22d2a4f6b43937e9c38f24
elogixDibyo <dibyojit@elogixmail.com>
2024-05-15 13:28:15 +0530

    click action on logo added

--- src/app/authentication/register/reg-header/reg-header.component.ts
+++ src/app/authentication/register/reg-header/reg-header.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reg-header',
   templateUrl: './reg-header.component.html',
   styleUrls: ['./reg-header.component.css'],
   standalone: true,
-  imports:[
+  imports: [
     MatDividerModule
   ]
 })
 export class RegHeaderComponent {
+  constructor( private router: Router) {}
+  handleClickOnLogo() {
+      this.router.navigate(['/home']);
 
+  }
 }
