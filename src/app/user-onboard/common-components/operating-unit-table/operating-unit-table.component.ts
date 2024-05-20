79c139bb5f2e5f81e3c274302e1fd5846f669fd0
elogixDibyo <dibyojit@elogixmail.com>
2024-05-20 11:14:54 +0530

    merged operating unit

--- src/app/user-onboard/common-components/operating-unit-table/operating-unit-table.component.ts
+++ src/app/user-onboard/common-components/operating-unit-table/operating-unit-table.component.ts
@@ -5,26 +5,26 @@
 //====================================== Testing Version ===========================//
 
 
-import { Component, ViewChild,Inject,OnInit   } from '@angular/core';
+import { Component, ViewChild, Inject, OnInit } from '@angular/core';
 
-import {MatTable, MatTableModule} from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatTable, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { CommonModule, NgStyle } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import {MatMenuTrigger, MatMenuModule} from '@angular/material/menu';
+import { MatMenuTrigger, MatMenuModule } from '@angular/material/menu';
 import { Router } from '@angular/router';
 import { Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { EmployeeCountCardComponent } from './employee-count-card/employee-count-card.component';
 
-import {ApiService} from '../../../services/api.service'
+import { ApiService } from '../../../services/api.service'
 
 import { DropdownComponent } from '../dropdown/dropdown.component';
 
@@ -48,7 +48,7 @@ import { OPUnitDetails } from 'src/app/shared/menu-items/operating-unit-details'
 
 
 const ELEMENT_DATA: OPUnitDetails[] = [
- 
+
 ];
 
 /**
@@ -59,16 +59,16 @@ const ELEMENT_DATA: OPUnitDetails[] = [
   templateUrl: 'operating-unit-table.component.html',
   styleUrls: ['./operating-unit-table.component.css'],
   standalone: true,
-  imports:[MatInputModule,MatCardModule,FormsModule,MatTableModule,NgStyle,
-          MatSelectModule,MatButtonModule,MatIconModule,MatMenuModule,
-          CommonModule,DropdownComponent,
-          AddNewOperatingUnitDialogComponent
-          ]
+  imports: [MatInputModule, MatCardModule, FormsModule, MatTableModule, NgStyle,
+    MatSelectModule, MatButtonModule, MatIconModule, MatMenuModule,
+    CommonModule, DropdownComponent,
+    AddNewOperatingUnitDialogComponent
+  ]
 })
 
-export class OperatingUnitTableComponent implements OnInit{
-  
-  constructor(public dialog: MatDialog,private router: Router, private fieldDefinitionService: ApiService) {}
+export class OperatingUnitTableComponent implements OnInit {
+
+  constructor(public dialog: MatDialog, private router: Router, private fieldDefinitionService: ApiService) { }
   @Input() entity = {
     "position": 1,
     "name": "Test Entity",
@@ -81,12 +81,12 @@ export class OperatingUnitTableComponent implements OnInit{
     "emailID": "",
     "laws": "",
     "lawModules": [
-        1,
-        2
+      1,
+      2
     ],
     "lawModulesLabel": [
-        "Labour",
-        "Operational"
+      "Labour",
+      "Operational"
     ],
     "operatingUnit": "",
     "actions": ""
@@ -100,31 +100,31 @@ export class OperatingUnitTableComponent implements OnInit{
       this.fetchstates();
     } else {
       this.states = [{
-        'id':1,
-        'name':'Singapore'
-      }]; 
+        'id': 1,
+        'name': 'Singapore'
+      }];
     }
     this.fetchOperatingUniTypes();
 
   }
 
-  fetchOperatingUniTypes(){
-  const fieldPayload=['operatingUnitTypes']
+  fetchOperatingUniTypes() {
+    const fieldPayload = ['operatingUnitTypes']
 
-  this.fieldDefinitionService.getFieldDefinition(fieldPayload).subscribe((response) => {
-   console.log('those are operating unit types',response)
-   this.operatingUnitTypes = response.data.operatingUnitTypes;
-  })
-}
+    this.fieldDefinitionService.getFieldDefinition(fieldPayload).subscribe((response) => {
+      console.log('those are operating unit types', response)
+      this.operatingUnitTypes = response.data.operatingUnitTypes;
+    })
+  }
 
-fetchstates(){
-  const fieldPayload=['states']
+  fetchstates() {
+    const fieldPayload = ['states']
 
-  this.fieldDefinitionService.getFieldDefinition(fieldPayload).subscribe((response) => {
-   console.log('those states',response)
-   this.states = response.data.states;
-  })
-}
+    this.fieldDefinitionService.getFieldDefinition(fieldPayload).subscribe((response) => {
+      console.log('those states', response)
+      this.states = response.data.states;
+    })
+  }
   displayedColumns: string[] = ['position', 'name', 'entity', 'ownership',
                                 'type', 'location', 'zone', 'employees','activities','laws','actions'];
 
@@ -132,21 +132,21 @@ fetchstates(){
 
   @ViewChild(MatTable) table: MatTable<OPUnitDetails>;
   @ViewChild('menuTrigger') menuTrigger: MatMenuTrigger;
- 
 
-  
+
+
   addOpUnitData(newData: OPUnitDetails) {
     newData['position'] = this.dataSource.length + 1
     this.dataSource.push(newData);
     this.table.renderRows();
-    
+
   }
-  
+
 
   rearrangeDataSource() {
-    
+
     this.dataSource.sort((a, b) => a.position - b.position);
-    
+
     for (let i = 0; i < this.dataSource.length; i++) {
       this.dataSource[i].position = i + 1;
     }
@@ -154,25 +154,29 @@ fetchstates(){
 
 
   removeEntityData(position: number) {
-   
+
     const rowIndex = this.dataSource.findIndex(row => row.position === position);
-  
-    
+
+
     if (rowIndex !== -1) {
-      
+
       this.dataSource.splice(rowIndex, 1);
-     
+
       this.rearrangeDataSource();
-      
+
       this.table.renderRows();
     }
   }
-  
+
   openEntityDialog(entityName: string) {
-    const dialogRef = this.dialog.open(AddNewOperatingUnitDialogComponent, {
-      data: { entityTable: this ,entityName: entityName, industry: this.entity.industryLabel,
-        operatingUnitTypes:this.operatingUnitTypes,
-      states:this.states}
+    console.log('entityName', entityName);
+
+    this.dialog.open(AddNewOperatingUnitDialogComponent, {
+      data: {
+        entityTable: this, entityName: entityName, industry: this.entity.industryLabel,
+        operatingUnitTypes: this.operatingUnitTypes,
+        states: this.states
+      }
     });
   }
 
@@ -183,17 +187,17 @@ fetchstates(){
     });
   }
 
-  navigateBackToEntityDetailsPage(){
-    this.router.navigate(['/entity-details'],{ state: this.entity }); 
+  navigateBackToEntityDetailsPage() {
+    this.router.navigate(['/entity-details'], { state: this.entity });
   }
 
 
-  openopUnitMenuDialog(action: string, position:number) {
-    console.log("ACTION SELECTED",action,position);
+  openopUnitMenuDialog(action: string, position: number) {
+    console.log("ACTION SELECTED", action, position);
 
-   
 
-    switch(action) {
+
+    switch (action) {
       case 'Delete':
         this.removeEntityData(position);
         break;
@@ -202,10 +206,10 @@ fetchstates(){
         break;
     }
 
-   
+
   }
-  
-  
+
+
 }
 
 
