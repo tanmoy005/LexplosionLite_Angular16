220f41d05f1e93183c0c533e4e604ab52af63013
anindita kar <aninditakar0714@gmail.com>
2024-06-04 12:20:43 +0530

    code-clinning

--- src/app/user-onboard/common-components/terms-condition-card/terms-condition-card.component.ts
+++ src/app/user-onboard/common-components/terms-condition-card/terms-condition-card.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-terms-condition-card',
   templateUrl: './terms-condition-card.component.html',
   styleUrls: ['./terms-condition-card.component.css'],
-  // standalone: true,
-  // imports: [MatCardModule, MatDividerModule, MatButtonModule, MatProgressBarModule],
+ 
 })
 export class TermsConditionCardComponent {
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
