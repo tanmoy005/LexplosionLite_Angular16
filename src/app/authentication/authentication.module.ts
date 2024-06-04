ec70440ad76cdeadf4728fff2b602aa80f3729f7
mrinalElogix72 <mrinal@elogixmail.com>
2024-06-04 11:20:43 +0530

    authentication module and routing cleaned.

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-// icons
-// import { TablerIconsModule } from 'angular-tabler-icons';
-// import * as TablerIcons from 'angular-tabler-icons/icons';
-
 import { AuthenticationRoutes } from './authentication.routing';
-
 import { AppSideLoginComponent } from '../guest-user/login/login.component';
 import { AppSideRegisterComponent } from './register/register.component';
 import { RegHeaderComponent } from './register/reg-header/reg-header.component';
 import { VerifyEmailPageComponent } from './register/verify-email-page/verify-email-page.component';
-import { MatDividerModule } from '@angular/material/divider';
 import { MaterialModule } from '../material-module';
 import { CommonComponentModule } from '../user-onboard/common-components/common-component.module';
-//import { BusinessCardComponent } from '../user-onboard/common-components/business-card/business-card.component';
 
 @NgModule({
   declarations: [
@@ -30,12 +17,6 @@ import { CommonComponentModule } from '../user-onboard/common-components/common-
   imports: [
     CommonModule,
     RouterModule.forChild(AuthenticationRoutes),
-    // MatIconModule,
-    // MatCardModule,
-    // MatInputModule,
-    // MatCheckboxModule,
-    // MatButtonModule,
-    // MatDividerModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
@@ -43,7 +24,6 @@ import { CommonComponentModule } from '../user-onboard/common-components/common-
     RegHeaderComponent,
     VerifyEmailPageComponent,
     CommonComponentModule    
-   // TablerIconsModule.pick(TablerIcons),
   ],
   
   exports: [
--- src/app/authentication/authentication.routing.ts
+++ src/app/authentication/authentication.routing.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
-
 import { AppSideRegisterComponent } from './register/register.component';
 import { VerifyEmailPageComponent } from './register/verify-email-page/verify-email-page.component';
 
 export const AuthenticationRoutes: Routes = [
-
   {
     path: 'register',
     component: AppSideRegisterComponent,
   },
   {
     path: 'verify-email',
-    component: VerifyEmailPageComponent
-  }
-
+    component: VerifyEmailPageComponent,
+  },
 ];
-
