b392e8ca188a108de6f34803f434bfca99317184
mrinalElogix72 <mrinal@elogixmail.com>
2024-06-19 10:47:08 +0530

    Registration page design changes.

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, ViewEncapsulation, OnInit, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, AbstractControl,ValidationErrors, ValidatorFn, FormBuilder  } 
-from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+  FormBuilder,
+} from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CountryList } from 'src/app/shared/menu-items/country-list';
 import { CountryData } from 'src/app/shared/menu-items/country-list';
 import { SnackbarService } from 'src/app/shared/snackbar.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
 
 function passwordValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
@@ -25,24 +31,34 @@ function passwordValidator(): ValidatorFn {
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
     const isValidLength = value.length >= 8;
 
-    const passwordValid = hasUpperCase && hasLowerCase && hasNumeric && hasSpecialChar && isValidLength;
+    const passwordValid =
+      hasUpperCase &&
+      hasLowerCase &&
+      hasNumeric &&
+      hasSpecialChar &&
+      isValidLength;
 
     return !passwordValid ? { passwordInvalid: true } : null;
   };
 }
 
-
-const passwordMatchValidator: ValidatorFn = (formGroup: AbstractControl): ValidationErrors | null => {
+const passwordMatchValidator: ValidatorFn = (
+  formGroup: AbstractControl
+): ValidationErrors | null => {
   const password = formGroup.get('password')?.value;
   const confirmPassword = formGroup.get('confirmPassword')?.value;
   //console.log("Password, Confirm password", password, confirmPassword);
-  const status =  password && confirmPassword && password === confirmPassword ? null : { passwordsMismatch: true };
+  const status =
+    password && confirmPassword && password === confirmPassword
+      ? null
+      : { passwordsMismatch: true };
 
   console.log(status);
 
-  return password && confirmPassword && password === confirmPassword ? null : { passwordsMismatch: true };
+  return password && confirmPassword && password === confirmPassword
+    ? null
+    : { passwordsMismatch: true };
 };
-
 
 @Component({
   selector: 'app-register',
@@ -51,13 +67,11 @@ const passwordMatchValidator: ValidatorFn = (formGroup: AbstractControl): Valida
   encapsulation: ViewEncapsulation.None,
 })
 export class AppSideRegisterComponent implements OnInit {
-  
   constructor(
     private router: Router,
     public dialog: MatDialog,
     private snackbar: SnackbarService,
     private fb: FormBuilder
-    
   ) {}
 
   usernameFormControl = new FormControl('', [Validators.required]);
@@ -67,25 +81,29 @@ export class AppSideRegisterComponent implements OnInit {
     Validators.email,
   ]);
 
-  headquarterAddressFormControl = new FormControl('',[Validators.required]);
+  headquarterAddressFormControl = new FormControl('', [Validators.required]);
 
   // passwordFormControl = new FormControl('', [Validators.required, passwordValidator()]);
   // confirmPasswordFormControl = new FormControl('',[Validators.required, passwordMatchValidator(this.passwordFormControl.value)])
   form: FormGroup;
 
   ngOnInit() {
-    this.form = this.fb.group({
-      password: ['', [Validators.required, passwordValidator()]],
-      confirmPassword: ['', [Validators.required]]
-    }, { validators: passwordMatchValidator });
+    this.form = this.fb.group(
+      {
+        password: ['', [Validators.required, passwordValidator()]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validators: passwordMatchValidator }
+    );
 
     this.passwordFormControl = this.form.get('password') as FormControl;
-    this.confirmPasswordFormControl = this.form.get('confirmPassword') as FormControl;
+    this.confirmPasswordFormControl = this.form.get(
+      'confirmPassword'
+    ) as FormControl;
   }
 
   passwordFormControl!: FormControl;
   confirmPasswordFormControl!: FormControl;
-
 
   phoneNumberFormControl = new FormControl('', [
     Validators.required,
@@ -103,7 +121,7 @@ export class AppSideRegisterComponent implements OnInit {
   get businessname() {
     return this.companynameFormControl.value;
   }
-  get headquarterAddress(){
+  get headquarterAddress() {
     return this.headquarterAddressFormControl.value;
   }
   get email() {
@@ -112,27 +130,24 @@ export class AppSideRegisterComponent implements OnInit {
   get password() {
     return this.passwordFormControl.value;
   }
-  get confirmPassword(){
+  get confirmPassword() {
     return this.confirmPasswordFormControl.value;
   }
- 
+
   get phoneNumber() {
     return this.phoneNumberFormControl.value;
   }
 
-
-
   countryCode: number | null = null;
   countryList: CountryData[] = CountryList;
 
-  updateCheckBoxStateAndOpenConditionModal(checkedStatus:boolean){
-      if(checkedStatus){
-        this.termsConditionModalOpen();
-      }
-      else{
-        this.isAgreeToTermsButtonClicked = false;
-        this.agreeToTerms = false;
-      }
+  updateCheckBoxStateAndOpenConditionModal(checkedStatus: boolean) {
+    if (checkedStatus) {
+      this.termsConditionModalOpen();
+    } else {
+      this.isAgreeToTermsButtonClicked = false;
+      this.agreeToTerms = false;
+    }
   }
 
   checkRegistrationCredentials() {
@@ -144,14 +159,14 @@ export class AppSideRegisterComponent implements OnInit {
       this.headquarterAddress?.trim() !== '' &&
       this.email?.trim() !== '' &&
       this.emailFormControl.valid &&
-      this.password.trim()!=='' &&
+      this.password.trim() !== '' &&
       this.passwordFormControl.valid &&
-      this.confirmPassword.trim()!==''&&
+      this.confirmPassword.trim() !== '' &&
       this.confirmPasswordFormControl.valid &&
       this.countryCode !== null &&
       this.phoneNumber?.trim() !== '' &&
       this.phoneNumberFormControl.valid &&
-      this.agreeToTerms 
+      this.agreeToTerms
       // &&
       // this.consentToPromotionalInfo
     ) {
@@ -186,15 +201,15 @@ export class AppSideRegisterComponent implements OnInit {
       data: { registrationPage: this },
     });
 
-    dialogRef.afterClosed().subscribe((termsAndConditionAcceptanceStatus:boolean)=> {
-      if(termsAndConditionAcceptanceStatus){
-        this.agreeToTerms = true;
-      }
-      else{
-        this.agreeToTerms = false;
-      }
-    });
-    
+    dialogRef
+      .afterClosed()
+      .subscribe((termsAndConditionAcceptanceStatus: boolean) => {
+        if (termsAndConditionAcceptanceStatus) {
+          this.agreeToTerms = true;
+        } else {
+          this.agreeToTerms = false;
+        }
+      });
   }
 }
 
@@ -217,26 +232,26 @@ function phoneNumberValidator(
   imports: [MatDialogModule, MatButtonModule],
 })
 export class TermsConditionDialog implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA)
-  public data: {
-    registrationPage: AppSideRegisterComponent;
-  },
-  public dialogRef: MatDialogRef<TermsConditionDialog>
-){ }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      registrationPage: AppSideRegisterComponent;
+    },
+    public dialogRef: MatDialogRef<TermsConditionDialog>
+  ) {}
 
   termsAgreeButtonClicked: boolean;
 
   ngOnInit(): void {
-    this.termsAgreeButtonClicked=false;
+    this.termsAgreeButtonClicked = false;
   }
-  
-  updateCheckBoxStatusAgreed(event:any){
+
+  updateCheckBoxStatusAgreed(event: any) {
     this.termsAgreeButtonClicked = true;
     this.dialogRef.close(this.termsAgreeButtonClicked);
   }
-  
-  updateCheckBoxStatusDeclined(event:any){
+
+  updateCheckBoxStatusDeclined(event: any) {
     this.termsAgreeButtonClicked = false;
     this.dialogRef.close(this.termsAgreeButtonClicked);
   }
@@ -244,5 +259,4 @@ export class TermsConditionDialog implements OnInit {
   closeDialog() {
     this.dialogRef.close(this.termsAgreeButtonClicked);
   }
-
 }
