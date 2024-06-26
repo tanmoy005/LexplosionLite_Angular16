b44bb6788c6bc5476b67f2d8d2ce34a8add66803
anindita kar <aninditakar0714@gmail.com>
2024-06-26 12:07:18 +0530

    company structure zoom functionality

--- src/app/user-onboard/common-components/company-structure-card/company-structure-card.component.ts
+++ src/app/user-onboard/common-components/company-structure-card/company-structure-card.component.ts
@@ -1,4 +1,18 @@
-import { Component,Input } from '@angular/core';
+// import { Component,Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-company-structure-card',
+//   templateUrl: './company-structure-card.component.html',
+//   styleUrls: ['./company-structure-card.component.scss']
+// })
+// export class CompanyStructureCardComponent {
+//  @Input() treeDataItem:any;
+//  companyStructureLabel = "Company Structure";
+//  companyStructureCardShade = "light"
+// }
+
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-company-structure-card',
@@ -6,7 +20,20 @@ import { Component,Input } from '@angular/core';
   styleUrls: ['./company-structure-card.component.scss']
 })
 export class CompanyStructureCardComponent {
- @Input() treeDataItem:any;
- companyStructureLabel = "Company Structure";
- companyStructureCardShade = "light"
+  @Input() treeDataItem: any;
+  //@Input() treeBase: number;
+  companyStructureLabel = "Company Structure";
+  companyStructureCardShade = "light";
+  zoomLevel: number = 1;
+
+  zoomIn() {
+    this.zoomLevel += 0.1;
+  }
+
+  zoomOut() {
+    this.zoomLevel -= 0.1;
+    if (this.zoomLevel < 0.1) {
+      this.zoomLevel = 0.1; 
+    }
+  }
 }
