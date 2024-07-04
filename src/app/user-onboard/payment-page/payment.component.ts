37769792f17b308a7ba4295375f1d2f2f1f2fc7e
anindita kar <aninditakar0714@gmail.com>
2024-07-04 13:04:39 +0530

    stepper modification

--- src/app/user-onboard/payment-page/payment.component.ts
+++ src/app/user-onboard/payment-page/payment.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { StepperComponent } from '../common-components/stepper/stepper.component';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss']
 })
-export class PaymentComponent {
+export class PaymentComponent  {
   constructor(private router: Router){ }
+  @ViewChild(StepperComponent, { static: false }) stepper: StepperComponent;
 
   colorPaymentDoneButton:string="#42C997";
+
+  // ngAfterViewInit(): void {
+  //   this.stepper.getStepControl(2).get('completed')?.setValue(true);
+  // }
 
   navigateToGoLivePage(event:any){
     this.router.navigate(['/golive']);
