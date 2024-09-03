1291e880d6e53743c5c7f0b2b3f58eefb8faf496
anindita kar <aninditakar0714@gmail.com>
2024-09-03 11:38:31 +0530

    Online & Offline Dialog

--- src/app/user-onboard/payment-page/offline-payment-dialog/offline-payment-dialog.component.ts
+++ src/app/user-onboard/payment-page/offline-payment-dialog/offline-payment-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offline-payment-dialog',
@@ -7,10 +8,17 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./offline-payment-dialog.component.scss'],
 })
 export class OfflinePaymentDialogComponent {
-  constructor(public dialogRef: MatDialogRef<OfflinePaymentDialogComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<OfflinePaymentDialogComponent>,
+    private router: Router
+  ) {}
   closeSuccessDialog() {
     console.log('close dialog clicked');
     // window.location.reload();
     this.dialogRef.close();
+  }
+  navigateToGoLivePage(event: any) {
+    this.dialogRef.close();
+    this.router.navigate(['/golive']);
   }
 }
--- src/app/user-onboard/payment-page/online-payment-dialog/online-payment-dialog.component.ts
+++ src/app/user-onboard/payment-page/online-payment-dialog/online-payment-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-online-payment-dialog',
@@ -7,10 +8,18 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./online-payment-dialog.component.scss'],
 })
 export class OnlinePaymentDialogComponent {
-  constructor(public dialogRef: MatDialogRef<OnlinePaymentDialogComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<OnlinePaymentDialogComponent>,
+    private router: Router
+  ) {}
   closeSuccessDialog() {
     console.log('close dialog clicked');
     // window.location.reload();
     this.dialogRef.close();
+  }
+
+  navigateToGoLivePage(event: any) {
+    this.dialogRef.close();
+    this.router.navigate(['/golive']);
   }
 }
