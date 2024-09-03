6a08a00b8ec44058aab29c55207890614c52a406
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-09-03 16:04:33 +0530

    Button text size

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'material-pro-angular-lite'`, () => {
+  it(`should have as title 'komrisk-lite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('material-pro-angular-lite');
+    expect(app.title).toEqual('komrisk-lite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('material-pro-angular-lite app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('komrisk-lite app is running!');
   });
 });
