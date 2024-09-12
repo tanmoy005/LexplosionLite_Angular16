1d058ef389738cb4a3025a68a2a76c72118b24c4
anindita kar <aninditakar0714@gmail.com>
2024-09-12 16:39:29 +0530

    Header New Design

--- src/app/authentication/register/reg-header/reg-header.component.ts
+++ src/app/authentication/register/reg-header/reg-header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-reg-header',
   templateUrl: './reg-header.component.html',
   styleUrls: ['./reg-header.component.css'],
   standalone: true,
-  imports: [MatDividerModule],
+  imports: [MatDividerModule, MatIconModule],
 })
 export class RegHeaderComponent {
   constructor(private router: Router) {}
