a2e66a42e934e6cf7934713d9525853da4ab346b
anindita kar <aninditakar0714@gmail.com>
2024-09-25 12:32:36 +0530

    Logo Change

--- src/app/user-onboard/subscription-details-page/feature-header/feature-header.component.ts
+++ src/app/user-onboard/subscription-details-page/feature-header/feature-header.component.ts
@@ -4,40 +4,37 @@ import { Input, Output } from '@angular/core';
 @Component({
   selector: 'app-feature-header',
   templateUrl: './feature-header.component.html',
-  styleUrls: ['./feature-header.component.scss']
+  styleUrls: ['./feature-header.component.scss'],
 })
 export class FeatureHeaderComponent {
- 
   checkboxItems = [
     {
-      imgSrc: './assets/images/logos/Komrisk logo.png',
+      imgSrc: './assets/images/logos/komrisk_logo_23-09-24.svg',
       checked: false,
       name: 'komrisk',
-      selectedImgSrc:'./assets/images/logos/Komrisk logowhite.png'
+      selectedImgSrc: './assets/images/logos/Property 1=Komrisk Logo L.svg',
     },
     {
-      imgSrc: './assets/images/logos/Komrisk Lite logo.png',
+      imgSrc: './assets/images/logos/Komrisk Lite Logo_2024.svg',
       checked: true,
       name: 'komriskLite',
-      selectedImgSrc:'./assets/images/logos/Komrisk Lite logoWhite.png'
-    }
+      selectedImgSrc: './assets/images/logos/Property 1=KL Lite.svg',
+    },
   ];
 
-  checkedSubscriptionType:string = 'komriskLite'; 
+  checkedSubscriptionType: string = 'komriskLite';
 
   @Output() selectedSubscriptionType = new EventEmitter<string>();
-  
+
   onCardClick(index: number): void {
-   
     this.checkboxItems.forEach((item, i) => {
       if (i !== index) {
         item.checked = false;
       }
     });
-  
+
     this.checkboxItems[index].checked = true;
     this.checkedSubscriptionType = this.checkboxItems[index].name;
     this.selectedSubscriptionType.emit(this.checkedSubscriptionType);
   }
-
 }
